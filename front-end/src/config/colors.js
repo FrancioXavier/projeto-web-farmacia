ccd70769d67a8b3306aa560e21d96e4af570f6b4
Ivna <ivnacontato.dev@gmail.com>
2024-02-03 20:01:27 -0300

    adding colors

--- front-end/src/config/colors.js
+++ front-end/src/config/colors.js
@@ -5,3 +5,33 @@ export const successColor = '#0197F6';
 export const infoColor = '#0197F6';
 export const errorColor = '#F2AF29';
 export const WarningColor = '#F2AF29';
+
+// our color palette
+// Brand Colors
+export const primaryLight = '#58C2FF';
+export const primaryDefault = '#4B80C5';
+export const primaryDark = '#004AAD';
+export const secondaryLight = '#89D99D';
+export const secondaryDefault = '#3B8C6E';
+export const secondaryDark = '#1E5959';
+// Neutral Colors
+export const highPure = '#FFFFFF';
+export const highLight = '#F1F3F5';
+export const highMedium = '#E9ECEF';
+export const highDark = '#DEE2E6';
+export const highDarkest = '#CED4DA';
+export const lowPure = '#000000';
+export const lowDarkest = '#343A40';
+export const lowDark = '#4950575';
+export const lowMedium = '#868E96';
+export const lowLight = '#ADB5BD';
+// Feedback Colors
+export const InformationLight = '#F4F9FF';
+export const InformationDefault = '#2678C2';
+export const InformationDark = '#0F304E';
+export const SuccessLight = '#F1FCF1';
+export const SuccessDefault = '#4ED34E';
+export const SuccessDark = '#269224';
+export const AlertLight = '#FEF0D8';
+export const AlertDefault = '#FBA409';
+export const AlertDark = '#C18E07';
