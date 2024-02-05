779cf678492d9424f6b9802f425b733426b7a4e0
FrancioXavier <francioxaviersilva@gmail.com>
2024-02-05 09:40:15 -0300

    Adiciono cores padrão do projeto

--- front-end/src/config/colors.js
+++ front-end/src/config/colors.js
@@ -1,7 +1,25 @@
-export const primaryColor = '#C3073F';
-export const primaryDarkColor = '#1A1A1D';
+export const primaryColor = '#4B80C5';
+export const primaryDarkColor = '#004AAD';
+export const primaryLightColor = '#58C2FF';
 
-export const successColor = '#0197F6';
-export const infoColor = '#0197F6';
-export const errorColor = '#F2AF29';
-export const WarningColor = '#F2AF29';
+export const secondaryColor  = '#3B8C6E';
+export const secondaryDarkColor = '#1E5959';
+export const secondaryLightColor = '#89D99D';
+
+export const successColor = '#4ED34E';
+export const successDarkColor = '#269224';
+
+export const infoColor = '#2678C2';
+export const infoDarkColor = '#0F304E';
+
+export const errorColor = '#FF1835';
+export const errorDarkColor = '#8F0D1E';
+
+export const WarningColor = '#FBA409';
+export const WarningDarkColor = '#C18E07';
+
+export const lightGray = '#ADB5BD';
+export const darkGray = '#495057';
+
+export const black = '#000000';
+export const white = '#FFF';
