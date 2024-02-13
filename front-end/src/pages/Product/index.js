71c615b2310e16697e9c7ef593011c060ce40dff
FrancioXavier <francioxaviersilva@gmail.com>
2024-02-13 14:45:22 -0300

    Sell product pages

---
+++ front-end/src/pages/Product/index.js
@@ -0,0 +1,8 @@
+import { Container } from '../../styles/GlobalStyles';
+export default function Product() {
+  return (
+    <Container>
+      <p>teste</p>
+    </Container>
+  );
+}
--- front-end/src/routes/index.js
+++ front-end/src/routes/index.js
@@ -4,11 +4,13 @@ import { Switch } from 'react-router-dom';
 import Login from '../pages/Login';
 import Page404 from '../pages/Page404';
 import MyRoute from './MyRoute';
+import Product from '../pages/Product';
 
 export default function Routes() {
   return (
     <Switch>
       <MyRoute exact path="/" component={Login} />
+      <MyRoute exact path="/teste" component={Product} />
       <MyRoute path="*" component={Page404} />
     </Switch>
   );
