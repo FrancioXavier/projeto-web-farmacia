be079b9d52391578eae1a0865d958c24fa82641a
FelipeFerraz4 <felipeferraz2402@gmail.com>
2024-02-26 00:29:42 -0300

    create contact register page

--- front-end/src/routes/index.js
+++ front-end/src/routes/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Switch } from 'react-router-dom';
 
+import ContactRegister from '../pages/ContactRegister';
 import Login from '../pages/Login';
 import Page404 from '../pages/Page404';
-import MyRoute from './MyRoute';
 import Product from '../pages/Product';
 import ShoppingCart from '../pages/ShoppingCart';
+import MyRoute from './MyRoute';
 
 export default function Routes() {
   return (
@@ -13,6 +14,7 @@ export default function Routes() {
       <MyRoute exact path="/" component={Login} />
       <MyRoute exact path="/product" component={Product} />
       <MyRoute exact path="/shopping-cart" component={ShoppingCart} />
+      <MyRoute exact path="/contactRegister" component={ContactRegister} />
       <MyRoute path="*" component={Page404} />
     </Switch>
   );
