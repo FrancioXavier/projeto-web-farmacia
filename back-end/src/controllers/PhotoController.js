8e90c2d24ef6a34e3223883237bf5256896c7305
FrancioXavier <francioxaviersilva@gmail.com>
2024-04-07 11:56:04 -0300

    photos incomplete

---
+++ back-end/src/controllers/PhotoController.js
@@ -0,0 +1,32 @@
+import multer from 'multer';
+import multerConfig from '../../config/multerConfig';
+import Photo from '../Models/Photo';
+const upload = multer(multerConfig).single('foto');
+
+class PhotoController {
+  async store(req, res) {
+    return upload(req, res, async (error) => {
+      req.body = '6612ad1a9673c9f94b5f63d3';
+      if (error) {
+        return res.status(400).json({
+          errors: [error.code],
+        });
+      }
+
+      try {
+        const { originalname, filename } = req.file;
+        const { productId } = req.body;
+        const foto = await Photo.create({ originalname, filename, productId });
+
+        return res.json(foto);
+      } catch (e) {
+        console.log(e);
+        return res.status(400).json({
+          errors: ['Produto não existe'],
+        });
+      }
+    });
+  }
+}
+
+export default new PhotoController();
